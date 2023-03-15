ebf80caf2aec6c067c90364d4afe58ac8b415a9e
Bilal Kaludi <63890666+Kaludii@users.noreply.github.com>
2023-03-15 00:49:35 -0500

    Uploading completed CSV Data Cleaning Tool from HuggingFace

---
+++ README.md
@@ -0,0 +1,61 @@
+# CSV Data Cleaning Tool
+
+This is a Streamlit app that helps users preprocess and clean CSV files quickly and stress-free. Users can upload one or multiple CSV files, and the app provides several options to merge, filter, and display the cleaned data.
+
+# Web App
+Click [Here](https://huggingface.co/spaces/Kaludi/CSV-Data-Cleaning-Tool_App "Here") To View This App Online!
+
+![image](https://user-images.githubusercontent.com/63890666/224580639-2aeec181-e7ad-446a-af69-0d2bfea33dff.png)
+
+## Features
+
+-   Upload one or multiple CSV files.
+-   Merge uploaded CSV files.
+-   Keep only the header of the first file.
+-   Remove duplicate rows.
+-   Remove empty rows.
+-   Choose end-line characters.
+-   Preview DataFrames before downloading.
+-   Download cleaned data as separate CSV files.
+
+## Usage
+
+After running the app, you will see a title and description explaining the app's purpose. You can then upload one or multiple CSV files using the file uploader. The app provides options for merging and cleaning the uploaded data.
+
+### Merging and Cleaning Options
+
+-   **Merge uploaded CSV files**: If you upload multiple files, you can merge them into a single DataFrame.
+-   **Keep only the header (first row) of the first file**: When merging files, you can choose to keep only the header of the first file and match columns from the other files.
+-   **Remove duplicate rows**: Remove any duplicate rows in the merged DataFrame.
+-   **Remove empty rows**: Remove any empty rows in the merged DataFrame.
+-   **End line**: Choose the end-line characters for your CSV files.
+
+### Preview and Download
+
+-   **Show DataFrames**: Preview the cleaned DataFrames before downloading.
+-   **Download cleaned data**: Download the cleaned data as separate CSV files or one big file.
+
+## Requirements
+
+-   Python 3.6 or higher
+-   Streamlit
+-   Pandas
+
+## Installation
+
+1.  Clone the repository:
+
+`git clone https://github.com/Kaludii/CSV-Data-Cleaning-Tool.git`
+
+2.  Install the required packages: To run this app, you will need to install the following dependencies or type `pip install -r requirements.txt` to automatically download:
+
+-   `streamlit`
+-   `pandas`
+
+You can install them using pip:
+
+`pip install streamlit pandas`
+
+3.  Run the app:
+
+`streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import io
+import base64
+
+st.set_page_config(page_title="Data Cleaning Tool", layout="wide")
+
+st.title("CSV Data Cleaning Tool")
+
+st.markdown("Upload one or multiple CSV files to preprocess and clean your files quickly and stress free.")
+
+uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
+
+dataframes = []
+
+if uploaded_files:
+    for file in uploaded_files:
+        file.seek(0)
+        df = pd.read_csv(file)
+        dataframes.append(df)
+
+    if len(dataframes) > 1:
+        merge = st.checkbox("Merge uploaded CSV files")
+
+        if merge:
+            # Merge options
+            keep_first_header_only = st.selectbox("Keep only the header (first row) of the first file", ["Yes", "No"])
+            remove_duplicate_rows = st.selectbox("Remove duplicate rows", ["No", "Yes"])
+            remove_empty_rows = st.selectbox("Remove empty rows", ["Yes", "No"])
+            end_line = st.selectbox("End line", ["\\n", "\\r\\n"])
+
+            try:
+                if keep_first_header_only == "Yes":
+                    for i, df in enumerate(dataframes[1:]):
+                        df.columns = dataframes[0].columns.intersection(df.columns)
+                        dataframes[i+1] = df
+
+                merged_df = pd.concat(dataframes, ignore_index=True, join='outer')
+
+                if remove_duplicate_rows == "Yes":
+                    merged_df.drop_duplicates(inplace=True)
+
+                if remove_empty_rows == "Yes":
+                    merged_df.dropna(how="all", inplace=True)
+
+                dataframes = [merged_df]
+
+            except ValueError as e:
+                st.error("Please make sure columns match in all files. If you don't want them to match, select 'No' in the first option.")
+                st.stop()
+
+    # Show or hide DataFrames
+    show_dataframes = st.checkbox("Show DataFrames", value=True)
+
+    if show_dataframes:
+        for i, df in enumerate(dataframes):
+            st.write(f"DataFrame {i + 1}")
+            st.dataframe(df)
+
+    if st.button("Download cleaned data"):
+        for i, df in enumerate(dataframes):
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="cleaned_data_{i + 1}.csv">Download cleaned_data_{i + 1}.csv</a>'
+            st.markdown(href, unsafe_allow_html=True)
+else:
+    st.warning("Please upload a CSV file.")
+    st.stop()
+
+st.markdown("")
+st.markdown("---")
+st.markdown("")
+st.markdown("<p style='text-align: center'><a href='https://github.com/Kaludii'>Github</a> | <a href='https://huggingface.co/Kaludi'>HuggingFace</a></p>", unsafe_allow_html=True)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
